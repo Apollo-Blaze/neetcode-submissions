1ba740f52ce6f6d4c3c0e4fd2c1aca8e35ef99ba
Srichand Suresh <133900084+Apollo-Blaze@users.noreply.github.com>
2026-06-06 22:31:11 +0530

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.cpp
@@ -0,0 +1,41 @@
+class PrefixTree {
+public:
+    struct TrieNode {
+        unordered_map<char, TrieNode*> children;
+        bool isEndOfWord = false;
+    };
+    
+    TrieNode* root;
+    
+    PrefixTree() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* curr = root;
+        for(char c : word){
+            if(!curr->children.count(c))
+                curr->children[c] = new TrieNode();
+            curr = curr->children[c];
+        }
+        curr->isEndOfWord = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* curr = root;
+        for(char c : word){
+            if(!curr->children.count(c)) return false;
+            curr = curr->children[c];
+        }
+        return curr->isEndOfWord; 
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* curr = root;
+        for(char c : prefix){
+            if(!curr->children.count(c)) return false;
+            curr = curr->children[c];
+        }
+        return true;  
+    }
+};
